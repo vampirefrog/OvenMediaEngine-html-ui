02a8c68738d56be27f71c38d690f174c33af3aa1
vampi <vampi@vampi.tech>
2024-04-26 12:10:51 +0300

    github link

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
 	<div class="container">
+	    <a href="https://github.com/vampirefrog/OvenMediaEngine-html-ui" target="_blank" class="float-end">GitHub ☍</a>
 		<h3>OME Client</h3>
 		<router-view></router-view>
 	</div>
